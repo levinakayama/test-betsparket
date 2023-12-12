62fa537f35413459b7f4052ea00ed5a1c02b4ac0
Levi Nakayama <levisn@gmail.com>
2023-12-12 00:10:33 -0300

    add effects in buckets

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
+
+  const [heightX, setHeightX] = useState('0%')
+  const [heightY, setHeightY] = useState('0%')
+  const [heightZ, setHeightZ] = useState('0%')
+
+  useEffect(()=>{
+    setHeightX('30%')
+    setHeightY('40%')
+    setHeightZ('70%')
+  },[setHeightX, setHeightY, setHeightZ])
 
   return (
     <div className='container'>
@@ -21,13 +33,19 @@ function App() {
       </div>
       <div className='row'>
         <div className='col-md-4 d-flex justify-content-center'>
-          <div className='bucket'></div>
+          <div className='bucket d-flex align-items-end'>
+            <div className='bucket-water' style={{height: heightX}}></div>
+          </div>
         </div>
         <div className='col-md-4 d-flex justify-content-center'>
-          <div className='bucket'></div>
+          <div className='bucket d-flex align-items-end'>
+            <div className='bucket-water' style={{height: heightY}}></div>
+          </div>
         </div>
         <div className='col-md-4 d-flex justify-content-center'>
-          <div className='bucket'></div>
+          <div className='bucket d-flex align-items-end'>
+            <div className='bucket-water' style={{height: heightZ}}></div>
+          </div>
         </div>
       </div>
       <div className='row mt-4'>
